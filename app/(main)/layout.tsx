b33fbd81be6f2ea5d8e8b925a27fb9a6022423a5
hef <ehfb>
2026-05-05 21:52:56 +0600

    fix: optimize analytics components placement and import

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from 'next/font/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
@@ -95,9 +95,9 @@ export default async function MainLayout({
             },
           }}
         />
+        <GoogleAnalytics gaId="G-9CVNBDEQG6" />
         <Analytics />
         <SpeedInsights />
-        <GoogleAnalytics gaId="G-9CVNBDEQG6" />
       </ThemeProvider>
     </div>
   );
