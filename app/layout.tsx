8e2459802365da7807a2908d4f7bef9bae152d9d
hef <ehfb>
2026-05-05 14:23:23 +0600

    feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/shared/ThemeProvider';
@@ -95,6 +96,7 @@ export default async function RootLayout({
             }}
           />
               <Analytics />
+              <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
