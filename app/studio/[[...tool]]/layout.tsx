e1f4f7bfbda50e183e2231aed87be4174bea5cbb
hef <ehfb>
2026-05-05 14:35:53 +0600

    fix: separate studio layout to prevent infinite reload

--- app/studio/[[...tool]]/layout.tsx
+++ app/studio/[[...tool]]/layout.tsx
@@ -4,8 +4,8 @@ export default function StudioLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
-    </html>
+    <div style={{ margin: 0, padding: 0, height: '100vh', width: '100vw' }}>
+      {children}
+    </div>
   );
 }
