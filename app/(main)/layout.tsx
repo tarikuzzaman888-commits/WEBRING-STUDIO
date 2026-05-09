85bb448baed20f64d53ae49722644164ecfc2306
hef <ehfb>
2026-05-10 03:22:26 +0600

    feat: complete mailchimp site connection and root script integration

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from 'next/font/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 import '../globals.css';
 import { ThemeProvider } from '@/components/shared/ThemeProvider';
 import Navbar from '@/components/layout/Navbar';
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 
 export default function RootLayout({
@@ -7,6 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="mcjs" strategy="afterInteractive">
+          {`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/30faced646e9e7815705d4dda/8ab535b9c79b21b90b93a407c.js");`}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
